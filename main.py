cd99aec0329db772b5e9d862fe4e946071605f90
CATALINA <oana.catalina.mihai@protonmail.com>
2024-06-06 10:28:57 +0200

    I fixed the 'deal extra card'

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from deck import Deck
 def main():
-    deck = Deck()
-    deck.cards_count()
-    dealt_card = deck.deal(2)
-    for card in dealt_card:
+    user_input = input('Would you like to play a game of Blackjack? y/n')
+    if user_input == 'n':
+        pass
+    else:
+        deck = Deck()
+        deck.cards_count()
+        dealt_cards = deck.deal(2)
+        for card in dealt_cards:
+            print(f'You are dealt a {card}')
+        print('The remaining cards in the deck are:')
+        for card in deck.deck:
+            print(card)
+        deck.cards_count()
+    user = input('Would you like to receive one more card? y/n')
+    if user == 'y':
+        dealt_card = deck.deal(1)
         print(f'You are dealt a {card}')
-    print('The remaining cards in the deck are:')
-    for card in deck.deck:
-        print(card)
+    else:
+        print('You chose not to get a new card')
     deck.cards_count()
+
+
 
 
 if __name__ == '__main__':
     main()
+
