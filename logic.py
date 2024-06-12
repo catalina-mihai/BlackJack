b972dcba8df03768587745a45697f51c127eabb2
tommydrengen <thomashalkiernicolajsen@gmail.com>
2024-06-12 13:13:41 +0200

    playerHand.py

--- logic.py
+++ logic.py
@@ -1,26 +1,38 @@
+import playerHand
 from deck import Deck
+from playerHand import PlayerHand
 
 
 class Logic:
     def __init__(self):
-        deck = Deck()
-        player = Player()
-        dealer = Dealer()
+        deck = Deck() # Creates 52 cards and shuffles
         playerScore = 0
         dealerScore = 0
         #player hand and score
         dealtCard = Deck.deal()
-        playerScore += dealtCard
+        player.draw(dealtCard)
+        playerScore += dealtCard.rank["value"]
         print(dealtCard)
         dealtCard = Deck.deal()
-        playerScore += dealtCard
+        playerScore += dealtCard.rank["value"]
         print(dealtCard)
+
         #dealer hand and score
         dealtCard = Deck.deal()
-        dealerScore += dealtCard
+        dealerScore += dealtCard.rank["value"]
         print(dealtCard)
         dealtCard = Deck.deal()
-        dealerScore += dealtCard
+        dealerScore += dealtCard.rank["value"]
+
+    def checkWinner(self, playerHand, dealerHand):
+        if playerHand.score == 21:
+            print("Player wins")
+        if dealerHand.score == 21:
+            print("Dealer wins")
+
+        if playerHand.PlayerHand.gameOver():
+            print("Game Over: Player loses (over 21)")
+
 
     #Shuffle
     #Deal 2 cards to each
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import deck
 from deck import Deck
+from logic import Logic
 def main():
     user_input = input('Would you like to play a game of Blackjack?\n y/n ')
     if user_input == 'n':
         pass
     else:
-        deck = Deck()
-        deck.cards_count()
-        dealt_card = deck.deal()
+        playerScore = 0
+        dealerScore = 0
+        logic = Logic()
+        # deck = Deck()
+        logic.deck.cards_count()
+        dealt_card = logic.deck.deal()
         print(f'You are dealt a {dealt_card}')
+        print(f'Your score is {dealt_card.rank}')
         print('The remaining cards in the deck are:')
-        for card in deck.deck:
+        for card in logic.deck.deck:
             print(card)
-        deck.cards_count()
-    user = input('Would you like to receive one more card?\n y/n ')
-    if user == 'y':
-        dealt_card = deck.deal()
+        logic.deck.cards_count()
+    userInput = input('Would you like to receive one more card?\n y/n ')
+    if userInput == 'y':
+        dealt_card = logic.deck.deal()
+        playerScore = playerScore + dealt_card.rank
         print(f'You are dealt a {dealt_card}')
     else:
         print('You chose not to get a new card')
