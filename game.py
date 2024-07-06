68fa786fc5207f5a58e42303ebc7d16f5dbce05e
CATALINA <oana.catalina.mihai@protonmail.com>
2024-07-06 19:09:58 +0200

    fixed error with empty spaces when input from user

---
+++ game.py
@@ -0,0 +1,91 @@
+from deck import Deck
+from hand import Hand
+class Game:
+    def play(self):
+        user_input = input('Would you like to play a game of Blackjack?\n Y,y/N,n ').lower().strip()
+        while user_input == 'y':
+            deck = Deck()
+            deck.shuffle()
+
+            player_hand = Hand()
+            dealer_hand = Hand(dealer=True)
+
+            for i in range(2):
+                player_hand.add_card(deck.deal(1))
+                dealer_hand.add_card(deck.deal(1))
+
+            print()
+            print("*" * 30)
+            print(f"BLACKJACK")
+            print("*" * 30)
+            player_hand.display()
+            dealer_hand.display()
+
+            if self.check_winner(player_hand, dealer_hand):
+                user_input = input('Would you like to play another game of Blackjack?\n Y,y/N,n ').lower().strip()
+                continue
+
+            choice = ""
+            while player_hand.get_value() < 21 and choice not in ["s", "stand"]:
+                choice = input("Please choose 'Hit' or 'Stand': ").lower().strip()
+                print()
+                while choice not in ["h", "s", "hit", "stand"]:
+                    choice = input("Please enter 'Hit' or 'Stand' (or H/S) ").lower().strip()
+                    print()
+                if choice in ["hit", "h"]:
+                    player_hand.add_card(deck.deal(1))
+                    player_hand.display()
+
+            if self.check_winner(player_hand, dealer_hand):
+                user_input = input('Would you like to play another game of Blackjack?\n Y,y/N,n ').lower().strip()
+                continue
+
+            player_hand_value = player_hand.get_value()
+            dealer_hand_value = dealer_hand.get_value()
+
+            while dealer_hand_value < 17:
+                dealer_hand.add_card(deck.deal(1))
+                dealer_hand_value = dealer_hand.get_value()
+
+            dealer_hand.display(show_all_dealer_cards=True)
+
+            if self.check_winner(player_hand, dealer_hand):
+                user_input = input('Would you like to play another game of Blackjack?\n Y,y/N,n ').lower().strip()
+                continue
+
+            print("Final Results")
+            print("Your hand:", player_hand_value)
+            print("Dealer's hand:", dealer_hand_value)
+
+            self.check_winner(player_hand, dealer_hand, True)
+
+            print("\nThanks for playing!")
+            user_input = input('Would you like to play another game of Blackjack?\n Y,y/N,n ').lower().strip()
+        print("\nGreat having you. Thanks for playing!")
+
+    def check_winner(self, player_hand, dealer_hand, game_over=False):
+        if not game_over:
+            if player_hand.get_value() > 21:
+                print("You busted. Dealer wins! 😭")
+                return True
+            elif dealer_hand.get_value() > 21:
+                print("Dealer busted. You win! 😀")
+                return True
+            elif dealer_hand.is_blackjack() and player_hand.is_blackjack():
+                print("Both players have blackjack! Tie! 😑")
+                return True
+            elif player_hand.is_blackjack():
+                print("You have blackjack. You win! 😀")
+                return True
+            elif dealer_hand.is_blackjack():
+                print("Dealer has blackjack. Dealer wins! 😭")
+                return True
+        else:
+            if player_hand.get_value() > dealer_hand.get_value():
+                print("You win! 😀")
+            elif player_hand.get_value() == dealer_hand.get_value():
+                print("Tie! 😑")
+            else:
+                print("Dealer wins. 😭")
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,35 +1,13 @@
-import deck
-from deck import Deck
-from logic import Logic
-def main():
-    user_input = input('Would you like to play a game of Blackjack?\n y/n ')
-    if user_input == 'n':
-        pass
-    else:
-        playerScore = 0
-        dealerScore = 0
-        logic = Logic()
-        # deck = Deck()
-        logic.deck.cards_count()
-        dealt_card = logic.deck.deal()
-        print(f'You are dealt a {dealt_card}')
-        print(f'Your score is {dealt_card.rank}')
-        print('The remaining cards in the deck are:')
-        for card in logic.deck.deck:
-            print(card)
-        logic.deck.cards_count()
-    userInput = input('Would you like to receive one more card?\n y/n ')
-    if userInput == 'y':
-        dealt_card = logic.deck.deal()
-        playerScore = playerScore + dealt_card.rank
-        print(f'You are dealt a {dealt_card}')
-    else:
-        print('You chose not to get a new card')
-    deck.cards_count()
+from game import Game
+from os import system, name
+from time import sleep
+def clear():
+      _ = system('cls')
+
+sleep(1)
+clear()
+game = Game()
+game.play()
 
 
-
-
-if __name__ == '__main__':
-    main()
-
+# Input Player (play again?) from Player
